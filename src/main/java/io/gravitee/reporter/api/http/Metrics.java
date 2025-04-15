f450d95135533d6725916b86a55c6139dce5123c
Michel Barret <michel.barret@graviteesource.com>
2025-04-15 11:01:17 +0200

    feat: create news metrics for AI
    APIM-9340

--- src/main/java/io/gravitee/reporter/api/http/Metrics.java
+++ src/main/java/io/gravitee/reporter/api/http/Metrics.java
@@ -21,7 +21,6 @@ import io.gravitee.reporter.api.log.Log;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
