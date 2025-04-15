f509fab7e0d70a1bda934f1c1843e5e5ec47c12f
vikrant <vikrant.kumar@graviteesource.com>
2025-04-15 17:40:59 +0530

    fix: update default response and latency time values from -1 to 0

--- src/main/java/io/gravitee/reporter/api/v4/metric/Metrics.java
+++ src/main/java/io/gravitee/reporter/api/v4/metric/Metrics.java
@@ -73,7 +73,7 @@ public class Metrics extends AbstractReportable {
     private String userAgent;
 
     @Builder.Default
-    private long requestContentLength = -1;
+    private long requestContentLength = 0;
 
     private boolean requestEnded;
     /**
@@ -86,7 +86,7 @@ public class Metrics extends AbstractReportable {
     private String endpoint;
 
     @Builder.Default
-    private long endpointResponseTimeMs = -1;
+    private long endpointResponseTimeMs = 0;
 
     /**
      * Response metrics
@@ -94,13 +94,13 @@ public class Metrics extends AbstractReportable {
     private int status;
 
     @Builder.Default
-    private long responseContentLength = -1;
+    private long responseContentLength = 0;
 
     @Builder.Default
-    private long gatewayResponseTimeMs = -1;
+    private long gatewayResponseTimeMs = 0;
 
     @Builder.Default
-    private long gatewayLatencyMs = -1;
+    private long gatewayLatencyMs = 0;
 
     /**
      * Security metrics
